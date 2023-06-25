104df851a5f77d639cf530b890137595dc4f4b13
agnusha <agnia2000@inbox.ru>
2023-06-25 23:41:07 +0100

    feat: switch to basic navigation handler

--- file-manager/src/index.js
+++ file-manager/src/index.js
@@ -1,14 +1,12 @@
-import { dirname, join } from "path";
-import { fileURLToPath } from "url";
-import { argv } from 'node:process';
+import { argv, cwd } from 'node:process';
 import { createInterface } from 'node:readline/promises'
 import { getUserName, welcome, goodbye } from './helpers/userHelper.js'
 import { printWorkingDirectory } from './helpers/fileHelper.js'
+import { goUp, goToDirectory, list } from './handlers/navigation.js'
 
 
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+let __dirname = cwd();
 const args = argv.slice(2);
 
 
@@ -16,15 +14,38 @@ try {
 
     const username = getUserName(args);
     welcome(username);
-    printWorkingDirectory();
 
     const readerLine = createInterface({
         input: process.stdin,
         output: process.stdout,
     });
 
+    printWorkingDirectory(__dirname);
     readerLine.prompt();
+
     readerLine
+        .on('line', async (line) => {
+            const [command, ...args] = line.trim().split(' ');
+            __dirname = cwd();
+
+            switch (command) {
+                case 'up':
+                    goUp(__dirname)
+                    break
+                case 'cd':
+                    goToDirectory(args[0])
+                    break
+                case 'ls':
+                    await list(__dirname)
+                    break
+
+                default: console.log('Invalid input')
+                    break
+            }
+
+            printWorkingDirectory(cwd());
+            readerLine.prompt();
+        })
         .on('SIGINT', () => readerLine.close())
         .on('close', () => goodbye(username));
 
